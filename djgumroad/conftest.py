3656d8f4475aa92253d7fbeb629ff79c71d53692
mattfreire <freire1996@gmail.com>
2021-01-27 11:29:35 +0200

    Created project

---
+++ djgumroad/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djgumroad.users.models import User
+from djgumroad.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
