decf7ea2f4c5e8c9f90474b217edfba0b61a92a3
mattfreire <freire1996@gmail.com>
2021-02-04 11:57:39 +0200

    Navbar and product image urls

--- djgumroad/products/views.py
+++ djgumroad/products/views.py
@@ -100,6 +100,9 @@ class CreateCheckoutSessionView(generic.View):
                 customer = request.user.stripe_customer_id
             else:
                 customer_email = request.user.email
+        product_image_urls = []
+        if product.cover:
+            product_image_urls.append(product.cover.url)
         session = stripe.checkout.Session.create(
             customer=customer,
             customer_email=customer_email,
@@ -110,6 +113,7 @@ class CreateCheckoutSessionView(generic.View):
                         'currency': 'usd',
                         'product_data': {
                             'name': product.name,
+                            'images': product_image_urls
                         },
                         'unit_amount': product.price,
                     },
